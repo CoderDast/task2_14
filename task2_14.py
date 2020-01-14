a4b57dff80f663e4580407209b59dd9a7b72612f
CoderDast <dastsultan@gmail.com>
2020-01-14 10:54:23 +0600

    task2_14

---
+++ task2_14.py
@@ -0,0 +1,14 @@
+my_list = input('Type numbers divided by space: ').split()
+for number in my_list:
+    if number % 2 == 1:
+        print (number)
+
+# import random
+# my_list = []
+# for x in range(20):
+#     my_list.append(random.randint(1, 1000))
+#     if my_list[x] % 2 == 0:\
+#         print (my_list[x]) 
+
+# my_list = input('Type numbers divided by space: ').split()
+# print (my_list)
